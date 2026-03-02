313f9ffa8b36ec5dc23348546c80ffbcaaab6177
Murad Hasan <mdmuradhasan151@gmail.com>
2026-03-02 14:39:11 +0600

    add testimonial section

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-$(document).ready(function(){
-      $(".slider").slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 0,
-        arrows: false,
-        dots: true,
-        speed: 3000,
-        centerMode: true,
-        centerPadding: "50px",
-        responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-      });
-    });
+$(document).ready(function () {
+  $(".slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 0,
+    arrows: false,
+    dots: true,
+    speed: 3000,
+    centerMode: true,
+    centerPadding: "50px",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+
+$(".fade").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+  cssEase: "linear",
+});
