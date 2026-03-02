d2af38f16c82d19996f130eac7b4c40a02e8af91
Murad Hasan <mdmuradhasan151@gmail.com>
2026-03-02 11:13:33 +0600

    70% done

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+      $(".slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        arrows: false,
+        dots: true,
+        speed: 3000,
+        centerMode: true,
+        centerPadding: "50px",
+        responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+      });
+    });
